6efcc0a8542e074903d5b7a64982a125e4d9bfe6
anu-007 <anubhutiv1@gmail.com>
2019-02-13 23:38:44 +0530

    node networking comments

--- nodeNetworking/net.js
+++ nodeNetworking/net.js
@@ -1,9 +1,14 @@
 process.stdout.write('\u001B[2J\u001B[0;0f');
 
 const server = require('net').createServer();
+
+// variable to set client id
 let counter = 0;
+
+// object to track open sockets
 let sockets = {};
 
+// calculates time stamp
 function timeStamp() {
     const now = new Date();
     return `(${now.getHours()}:${now.getMinutes()})`
@@ -15,7 +20,9 @@ server.on('connection', (socket) => {
     console.log('a client is connected');
     socket.write('please enter your name');
 
+    // on data event from client
     socket.on('data', data => {
+        // for first time when client connects (asks hi/her name)
         if(!sockets[socket.id]) {
             socket.name = data.toString().trim();
             socket.write(`welcome to the chat app ${socket.name} \n`);
@@ -23,13 +30,16 @@ server.on('connection', (socket) => {
             return;
         }
 
+        // broadcast msg from client to all the open sockets
         Object.entries(sockets).forEach(([key, cs]) => {
             if(socket.id == key) return;
             cs.write(`${socket.name} : ${timeStamp()} : ${data}`); 
         });
     });
 
+    // handler for client disconnect event
     socket.on('end', () => {
+        // delete disconnected client from the open sockets object
         delete sockets[socket.id];
         console.log('bye');
     });
